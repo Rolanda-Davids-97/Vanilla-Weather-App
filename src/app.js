e3b55035707515aac76530896dcec8424ed996ec
Rolanda-Davids-97 <davidsrolanda@yahoo.com>
2023-09-14 09:35:14 +0200

    added real life data to index.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+function displayTemperature(response) {
+  console.log(response.data.condition.description);
 
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+}
+
+let apiKey = "4t804o3f400bde415f63abf53543fcd3";
+let city = "Johannesburg";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
